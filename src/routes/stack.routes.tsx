bdf2f103769846a88ce09ad7368f2fed66988d77
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-01-23 02:56:45 -0300

    feat: routes file added

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -3,17 +3,17 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const { Screen, Navigator } = createNativeStackNavigator();
 
 // importando as telas
-import { 
-  ScreenA,
-  ScreenB
-} from '@screens/index'
+import * as screen from '@screens/index';
 
 
 export const StackRoutes = () => {
   return(
-    <Navigator>
-      <Screen name="ScreenA" component={ScreenA} />
-      <Screen name="ScreenB" component={ScreenB} />
+    <Navigator screenOptions={{ headerShown: false }}>
+
+      <Screen name="screenA" component={screen.ScreenA} />
+      <Screen name="screenB" component={screen.ScreenB} />
+      <Screen name="card" component={screen.Card} />
+    
     </Navigator>  
-  )
+  );
 };
