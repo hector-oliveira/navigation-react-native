d3628cc1cb3603081506d2fa5493215f97e215a3
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-01-23 03:02:41 -0300

    feat: new theme added

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -6,12 +6,17 @@ export default {
     secondary_900: '#FF872C',
     secondary_800: '#E6A16C',
 
+    complementary_900: '#E2F75C',
+    complementary_800: '#F7FF9E',
+
     shape: '#FFFFFF',
     title: '#8355d8',
     text: '#969CB2',
     
     background_first: '#1C0D25',
     background_second: '#0d2525',
+
+    complementary_bac_second: '#DE5A0D',
   },
 
   FONTS: {
